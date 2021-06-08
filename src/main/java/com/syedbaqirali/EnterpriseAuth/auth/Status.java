28112dbe6017959900c3a03347d214a4dbef4730
SBee2020 <gitbaq@gail.com>
2021-06-08 17:29:16 +0300

    Initial SBA

---
+++ src/main/java/com/syedbaqirali/EnterpriseAuth/auth/Status.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.syedbaqirali.EnterpriseAuth.auth;
+
+/**
+ * @author rever
+ *
+ */
+public enum Status {
+	SUCCESS, USER_ALREADY_EXISTS, FAILURE
+
+}
---
+++ src/test/java/com/syedbaqirali/EnterpriseAuth/EnterpriseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syedbaqirali.EnterpriseAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
